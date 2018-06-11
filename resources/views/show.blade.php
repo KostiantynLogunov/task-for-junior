944d0b77de6858af3e82855dd5049e489728f0e1
Kostiantyn Logunov <777koc@gmail.com>
2018-06-11 16:30:07 +0100

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,12 +6,12 @@
 
         <div  class="container">
                         <header class="text-center">
-                            <h2>{{ $product->title }}</h2>
+                            <h2>{{ $product['title'] }}</h2>
                         </header>
                         <div class="image1  text-center">
-                            <img  src="/images/{{$product->image}}" alt="{{$product->image}}" style="max-height: 500px;">
+                            <img  src="/images/{{$product['image']}}" alt="{{$product['image']}}" style="max-height: 500px;">
                         </div>
-                        <p>{!! $product->description !!}</p>
+                        <p>{!! $product['description'] !!}</p>
                         <a href="{{ route('home.index') }}" class="button">Back</a>
         </div>
 
